687f7bf1c9c80b4037fd69cfb0a7e881068ebb30
joaocastro <joaocastro0429@gmail.com>
2024-09-27 05:33:04 -0300

    atualizando

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_23_012627) do
+ActiveRecord::Schema.define(version: 2024_09_27_081542) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
