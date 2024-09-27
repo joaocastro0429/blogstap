a92073b2d56dd7e0458b143408dec2e41a47002c
joaocastro <joaocastro0429@gmail.com>
2024-09-27 05:12:45 -0300

    add listando

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_23_003440) do
+ActiveRecord::Schema.define(version: 2024_09_23_012627) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
