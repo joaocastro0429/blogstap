161759768a46a7c609e24efeeb823687b46a7567
joaocastro <joaocastro0429@gmail.com>
2024-09-27 21:26:56 -0300

    add rotas do edit e update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,30 +1,44 @@
 class ArticlesController < ApplicationController
-    def index
-        @articles = Article.all
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    puts params[:id]  # ou use logger.debug params[:id]
+    @article = Article.find(params[:id])
+  end
+  
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
     end
-    
-    def show
-        @article= Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def new
-        @article=Article.new
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
     end
-
-    def create
-        @article = Article.new(article_params)
-        
-        if @article.save
-          redirect_to @article
-        else
-          render :new
-        end
-      end
-
-      private 
-
-      def article_params
-        params.require(:article).permit(:title, :body)
-      end
-      
 end
